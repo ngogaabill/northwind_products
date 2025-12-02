74c7edbe64f3d2c11cdd630b01f54801c5307a0d
Billngoga <ngogaabill@example.com>
2025-12-02 09:38:26 -0600

    separated query methods and used Datasource

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,38 @@
-
+import org.apache.commons.dbcp2.BasicDataSource;
 import java.sql.*;
 import java.util.Scanner;
-
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws SQLException {
-        String query = "";
-        boolean running = true;
 
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/northwind");
+        dataSource.setUsername("root");
+        dataSource.setPassword("yearup");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+        boolean running = true;
         while (running) {
             int choice = homeScreen();
             switch (choice) {
                 case 1:
-                    query = "SELECT * FROM products";
-                    displayResults(query, choice);
+                    queryProducts(dataSource);
                     break;
                 case 2:
-                    query = "SELECT * FROM customers WHERE country is not NUll ORDER BY country  ";
-                    displayResults(query, choice);
+                    queryCustomers(dataSource);
                     break;
                 case 3:
-                    query = "SELECT * FROM categories";
-                    displayResults(query, choice);
-                    searchCategory();
+                    queryCategories(dataSource);
+                    searchCategory(dataSource);
                     break;
                 case 0:
                     exit();
                     running = false;
-                    return;
+                    break;
             }
         }
-
-    }
-
-    private static void searchCategory() throws SQLException {
-        System.out.println("Enter categoryID to search: ");
-        int id = scanner.nextInt();
-        String query = "SELECT ProductID,ProductName,UnitPrice,UnitsInStock FROM Products WHERE CategoryID = " + id;
-        displayResults(query, 4);
-    }
-
-    private static void exit() {
-        System.out.println("Good bye");
     }
 
     private static int homeScreen() {
@@ -54,55 +43,88 @@ public class Main {
                 3) Display all categories
                 0) Exit
                 Select an option:""");
-
-        int choice = scanner.nextInt();
-        return choice;
+        return scanner.nextInt();
     }
 
-    public static void displayResults(String query, int choice) throws SQLException {
+    private static void exit() {
+        System.out.println("Good bye");
+    }
 
-        try (
-                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "yearup");
-                PreparedStatement statement = connection.prepareStatement(query);
-                ResultSet resultSet = statement.executeQuery()
-        ) {
+    public static void queryProducts(BasicDataSource dataSource) throws SQLException {
+        String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
 
             while (resultSet.next()) {
-                if (choice == 1) {
-                    int id = resultSet.getInt("ProductID");
-                    String name = resultSet.getString("ProductName");
-                    double price = resultSet.getDouble("UnitPrice");
-                    int stock = resultSet.getInt("UnitsInStock");
+                int id = resultSet.getInt("ProductID");
+                String name = resultSet.getString("ProductName");
+                double price = resultSet.getDouble("UnitPrice");
+                int stock = resultSet.getInt("UnitsInStock");
 
-                    System.out.println("Product Id: " + id);
-                    System.out.println("Name: " + name);
-                    System.out.println("Price: " + price);
-                    System.out.println("Stock: " + stock);
-                    System.out.println("------------------");
+                System.out.println("Product Id: " + id);
+                System.out.println("Name: " + name);
+                System.out.println("Price: " + price);
+                System.out.println("Stock: " + stock);
+                System.out.println("------------------");
+            }
+        }
+    }
+    public static void queryCustomers(BasicDataSource dataSource) throws SQLException {
+        String query = "SELECT ContactName, CompanyName, City, Country, Phone " +
+                "FROM Customers WHERE Country IS NOT NULL ORDER BY Country";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
 
-                } else if (choice == 2) {
-                    String contactName = resultSet.getString("ContactName");
-                    String companyName = resultSet.getString("CompanyName");
-                    String city = resultSet.getString("City");
-                    String country = resultSet.getString("Country");
-                    String phone = resultSet.getString("Phone");
+            while (resultSet.next()) {
+                String contactName = resultSet.getString("ContactName");
+                String companyName = resultSet.getString("CompanyName");
+                String city = resultSet.getString("City");
+                String country = resultSet.getString("Country");
+                String phone = resultSet.getString("Phone");
 
-                    System.out.println("Company Name: " + companyName);
-                    System.out.println("Name: " + contactName);
-                    System.out.println("City: " + city);
-                    System.out.println("Country: " + country);
-                    System.out.println("Phone: " + phone);
-                    System.out.println("------------------");
-                } else if (choice == 3) {
-                    int categoryId = resultSet.getInt("CategoryID");
-                    String categoryName = resultSet.getString("CategoryName");
-                    System.out.println("Category ID: " + categoryId);
-                    System.out.println("Category Name: " + categoryName);
-                    System.out.println("------------------");
-                } else if (choice == 4) {
+                System.out.println("Company Name: " + companyName);
+                System.out.println("Name: " + contactName);
+                System.out.println("City: " + city);
+                System.out.println("Country: " + country);
+                System.out.println("Phone: " + phone);
+                System.out.println("------------------");
+            }
+        }
+    }
+
+    public static void queryCategories(BasicDataSource dataSource) throws SQLException {
+        String query = "SELECT CategoryID, CategoryName FROM Categories";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                int categoryId = resultSet.getInt("CategoryID");
+                String categoryName = resultSet.getString("CategoryName");
+
+                System.out.println("Category ID: " + categoryId);
+                System.out.println("Category Name: " + categoryName);
+                System.out.println("------------------");
+            }
+        }
+    }
+
+    private static void searchCategory(BasicDataSource dataSource) throws SQLException {
+        System.out.println("Enter categoryID to search: ");
+        int id = scanner.nextInt();
+        String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock " +
+                "FROM Products WHERE CategoryID = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
                     int productId = resultSet.getInt("ProductID");
                     String productName = resultSet.getString("ProductName");
-                    Double price = resultSet.getDouble("UnitPrice");
+                    double price = resultSet.getDouble("UnitPrice");
                     int unitsInStock = resultSet.getInt("UnitsInStock");
 
                     System.out.println("Product ID: " + productId);
@@ -110,14 +132,8 @@ public class Main {
                     System.out.println("Unit Price: " + price);
                     System.out.println("Units In Stock: " + unitsInStock);
                     System.out.println("------------------");
-
                 }
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 }
-
-
-
